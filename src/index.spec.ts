f26dff109e2e6ba6883c0d49a9379ebf03e13cff
Davide Cavaliere <cavaliere.davide@gmail.com>
2018-08-19 13:11:36 +0000

    first commit

---
+++ src/index.spec.ts
@@ -0,0 +1,8 @@
+// tslint:disable:no-expression-statement no-object-mutation
+
+import test from 'ava';
+
+test('serverless-apigator', (t) => {
+  t.is(true, true);
+
+});
--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,100 @@
-export * from './lib/async';
-export * from './lib/hash';
-export * from './lib/number';
+import { getServiceMetadata, getEndpointMetadata} from '@microgamma/apigator';
+import { getDebugger } from '@microgamma/ts-debug';
+
+
+const debug = getDebugger('microgamma:serveless-apigator');
+
+class Serverless {
+
+
+  private servicePath: string;
+  private artifactsPath: string;
+  private entrypoint: string;
+  private apiFolder: string;
+
+
+  public hooks: any = {};
+
+
+  constructor(private serverless: any, private options: any) {
+
+    serverless.cli.log('-------------------------------------');
+    serverless.cli.log('Parsing Service definitions');
+    this.servicePath = serverless.config.servicePath;
+    debug('servicePath:', this.servicePath);
+
+    const awsService = serverless.service.service;
+    debug('awsService name', awsService);
+
+    const services = serverless.service.custom.services;
+    debug('pre defined services', services);
+    this.artifactsPath = serverless.service.custom.artifactsFolder;
+    this.apiFolder = serverless.service.custom.apiFolder;
+    this.entrypoint = serverless.service.custom.entrypoint;
+    debug('entrypoint', this.entrypoint);
+
+    this.hooks = {
+      'before:package:initialize': () => {
+        debug('before:package:initialize');
+        return this.configureFunctions();
+      },
+
+      'before:invoke:local:invoke': () => {
+        debug('before:invoke:local:invoke');
+        return this.configureFunctions();
+      }
+    };
+  }
+
+  public configureFunctions() {
+
+    debug('importing module');
+
+    // const module = require(`${this.servicePath}/${this.entrypoint}`);
+    const modulePath = `${this.servicePath}/${this.entrypoint}`;
+    return import(modulePath).then((module) => {
+
+      this.serverless.cli.log('Injecting configuration');
+      debug('works', module);
+
+      const service = module.default;
+
+      debug('metadata', Reflect.getMetadataKeys(service));
+      debug('metadata', Reflect.getMetadata('Endpoint', service));
+      debug('Service', getServiceMetadata(service));
+      debug('Endpoints', getEndpointMetadata(service));
+
+      const endpoints = getEndpointMetadata(service);
+
+      for (const endpoint of endpoints) {
+        debug('configuring endpoint', endpoint);
+        const functionName = endpoint.name;
+
+        this.serverless.service.functions[endpoint.name] = {
+          handler: `${this.entrypoint}.${functionName}`,
+          events: [
+            {
+              http:  {
+                path: endpoint.path,
+                method: endpoint.method,
+                integration: 'lambda',
+                cors: true
+              }
+            }
+          ]
+        }
+
+        debug('functions is');
+        debug(this.serverless.service.functions[endpoint.name]);
+        debug(this.serverless.service.functions[endpoint.name].events);
+      }
+
+      this.serverless.cli.log(`${endpoints.length} endpoints configured`);
+
+    }).catch((err) => {
+      console.error(err);
+    });
+  }
+}
+
+export = Serverless;
